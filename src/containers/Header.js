d68a6b4202006b94964e3006a60efd62aba8eb80
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-07-21 18:50:37 -0400

    add gradient

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { SwitchToggle } from "../components";
 
 import {
@@ -8,13 +8,11 @@ import {
   BgHeaderDesktop,
   IconMoon,
   IconSun,
-  IconSearch,
-  IconFilter,
 } from "../constants/images";
 
 const Header = ({ width }) => {
   return (
-    <header className="bg-transparent relative px-6 pt-8 md:pt-11 h-[136px] md:h-[160px] lg:h-[162px] overflow-x-hidden">
+    <header className="bg-gradient-to-l from-violet via-violet relative px-6 pt-8 md:pt-11 h-[136px] md:h-[160px] lg:h-[162px] overflow-x-hidden">
       <div className="lg:max-w-[1110px] lg:mx-auto">
         <div className="absolute z-0 top-0 left-0 w-full">
           {width > 767 && width < 1024 ? (
